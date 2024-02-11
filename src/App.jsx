c9a582a4d40d5eab825c35476fe5b21c34785afa
AZTechDevelop <157212350+AZTechDevelop@users.noreply.github.com>
2024-02-11 13:45:07 +0000

    css added

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,38 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const[word, setWord] = useState('')
-  const[list, setList] = useState([])
+ const[word, setWord] = useState('')
+ const[list,setList] = useState([])
 
-  const addWordToList = () => {
-    if(word){
-      setList(prevList => [...prevList, word])
-      setWord('')
-    }
+const addWordToList = () => {
+  if(word){
+    setList(prevList => [...prevList, word])
+    setWord('')
+  }
+}
+
+const quickSort = (arr, left=0, right=arr.length -1) => {
+  if(left < right){
+    let pi = partition(arr,left,right) // pivotIndex
+
+    quickSort(arr,left,pi-1)
+    quickSort(arr,pi+1,right)
   }
 
-  const quickSort = (arr,left=0, right = arr.length - 1) => {
-    if(left < right){
-      let pivotIndex = partition(arr,left,right)
-
-      quickSort(arr, left, pivotIndex - 1)
-      quickSort(arr, pivotIndex+1, right)
-    }
-    return arr;
-  }
-
-  const partition = (arr, left, right) => {
-    let pivot = arr[right]
-    let i = left - 1
+  return arr
+}
+  const partition = (arr,left,right) => {
+    let p = arr[right] //pivot
+    let i = left-1
 
     for(let j=left ; j<right ; j++){
-      if(arr[j].localeCompare(pivot) < 0){
+      if(arr[j].localeCompare(p) < 0){
         i++;
         [arr[i], arr[j]] = [arr[j], arr[i]]
       }
     }
-    [arr[i+1], arr[right]] = [arr[right], arr[i+1]]
-    return i+1
-
+  [arr[i+1], arr[right]] = [arr[right], arr[i+1]]
+  return i+1
   }
   const sortAscending = () => {
     setList(prevList => quickSort([...prevList]))
@@ -43,20 +42,22 @@ function App() {
   const sortDescending = () => {
     setList(prevList => quickSort([...prevList]).reverse())
   }
+
   return (
     <div className="App">
-      <input
-        type="text"
-        value={word}
-        onChange={(e) => setWord(e.target.value)}
-        placeholder="Introduceți un cuvânt"
+      <input 
+      type="text"
+      placeholder="Enter a word"
+      value={word}
+      onChange={(el) => setWord(el.target.value)}
       />
-      <button onClick={addWordToList}>Adaugă Cuvânt</button>
-      <button onClick={sortAscending}>Ordonează Crescător</button>
-      <button onClick={sortDescending}>Ordonează Descrescător</button>
+
+      <button onClick={addWordToList} >Add New Word</button>
+      <button onClick={sortAscending}>Sort ascending</button>
+      <button onClick={sortDescending}>Sort Descending</button>
       <ul>
-        {list.map((item, index) => (
-          <li key={index}>{item}</li>
+        {list.map((item) => (
+          <li key={item}>{item}</li>
         ))}
       </ul>
     </div>
